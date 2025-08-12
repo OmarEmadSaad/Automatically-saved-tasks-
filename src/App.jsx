15eb1b886c31134455ba5f4add009752546bfeda
OmarEmadSaad <oemad3987@gmail.com>
2025-08-12 16:46:42 +0300

    edite

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
                   Redux Powered
                 </div>
                 <a
-                  href="https://github.com"
+                  href="https://github.com/OmarEmadSaad/Automatically-saved-tasks-.git"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-all duration-200"
